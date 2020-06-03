a522108347f23de7228f44e8363cc9ba26293860
wilson sousa <fraga.sousa@bol.com.br>
2020-06-03 13:43:49 -0300

    teste deleção de linhas

--- Program.cs
+++ Program.cs
@@ -8,16 +8,7 @@ namespace Olá_mundo
         {
             Console.WriteLine("Hello--++---5-------- World!");
             Console.WriteLine("Hello--++----------- World!");
-            Console.WriteLine("Hello--++----5------- World!");
-            Console.WriteLine("Hello--++----------- World!");
-            Console.WriteLine("Hello--++----------- World!");
-            Console.WriteLine("Hello--++----------- World!");
-            Console.WriteLine("Hello--++---5-------- World!");
-            Console.WriteLine("Hello--++----------- World!");
-            Console.WriteLine("Hello--++----5------- World!");
-            Console.WriteLine("Hello--++----------- World!");
-
-        }
+              }
     }
 
 
